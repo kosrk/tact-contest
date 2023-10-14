a3a8e97bd2a95116fb4bb8d9e7a9114c892d9407
Alexey Kostenko <alexey.a.kostenko@gmail.com>
2023-10-14 22:36:35 +0300

    task1

--- tests/Task1.spec.ts
+++ tests/Task1.spec.ts
@@ -1,6 +1,7 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
 import { toNano } from 'ton-core';
 import { Task1 } from '../wrappers/Task1';
+import '@ton-community/test-utils';
 
 describe('Task1', () => {
     let blockchain: Blockchain;
@@ -28,6 +29,48 @@ describe('Task1', () => {
         });
     });
 
-    it('test', async () => {
+    it('should deploy', async () => {
+        // the check is done inside beforeEach
+        // blockchain and fireworks are ready to use
     });
+
+    it("should get counter value", async () => {
+        const value = await task1.getCounter();
+        expect(value).toEqual(0n);
+    });
+
+    it("should increment the counter value", async () =>  {
+        const deployer = await blockchain.treasury('deployer');
+        await task1.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05'),
+            },
+            {
+                $$type: 'Add',
+                queryId: 0n,
+                number: 1n,
+            }
+        );
+        const value = await task1.getCounter();
+        expect(value).toEqual(1n);
+    })
+
+    it("should decrease the counter value", async () =>  {
+        const deployer = await blockchain.treasury('deployer');
+        await task1.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05'),
+            },
+            {
+                $$type: 'Subtract',
+                queryId: 0n,
+                number: 1n,
+            }
+        );
+        const value = await task1.getCounter();
+        expect(value).toEqual(-1n);
+    })
+
 });
